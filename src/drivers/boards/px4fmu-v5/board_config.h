0089f210e8d4e596adb409d495be2f9dc1e6a33a
mynameiswwh <754009586@qq.com>
2017-10-31 13:54:08 +0800

    Update board_config.h

--- src/drivers/boards/px4fmu-v5/board_config.h
+++ src/drivers/boards/px4fmu-v5/board_config.h
@@ -1,5 +1,5 @@
 /****************************************************************************
- *111111111111111111111111
+ *
  *   Copyright (c) 2016 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
